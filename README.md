14ff1a6aacd81e48c6f6b12421caea9376571b16
Sergii Bilyi <kartanisoid@gmail.com>
2020-04-20 18:09:45 +0300

    minor updates

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # leetcoder
+
+Solving from https://leetcode.com/explore/challenge/card/30-day-leetcoding-challenge/528/week-1/3284/
--- src/single_number.go
+++ src/single_number.go
@@ -22,7 +22,6 @@ func main() {
 	input := []int{1,2,2,1,4}
 	invoke(input, 4, "for %d unique value expected %d and was %d")
 	invoke([]int{3,5,3,4,4,7,8,7,8}, 5, "for %d unique value expected %d and was %d")
-	// invoke(input, 4, "for %d unique value expected %d and was %d")
 }
 
 func invoke(input  []int, expected int, pattern string) {
